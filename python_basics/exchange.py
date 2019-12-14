c8d6288f36107508cc765a324792f7443dc26df2
uzchu <spdlqjtop@naver.com>
2019-12-14 17:39:41 +0900

    Deleated files

--- python_basics/exchange.py
+++
@@ -1,7 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-response = requests.get('https://finance.naver.com/marketindex/').text
-soup = BeautifulSoup(response,'html.parser')
-exchange = soup.select_one('#exchangeList > li.on > a.head.usd > div > span.value').text
-print(exchange)
--- python_basics/kospi.py
+++
@@ -1,9 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-response = requests.get('https://finance.naver.com/sise/').text
-soup = BeautifulSoup(response, 'html.parser')
-#print(type(response))
-#print(type(soup))
-kospi = soup.select_one('#KOSPI_now').text
-print(kospi)
